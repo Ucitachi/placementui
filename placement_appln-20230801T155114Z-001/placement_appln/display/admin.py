62ce6a3b5b0ecb98367565fed710b99f011611d6
Itachi <pavanshetty742@gmail.com>
2023-08-15 00:28:15 +0530

    pavan says hi

--- placement_appln-20230801T155114Z-001/placement_appln/display/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Company, Updates
-# Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(Company)
-admin.site.register(Updates)
